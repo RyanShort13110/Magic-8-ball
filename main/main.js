76d6a1fc8a52de6983d04bb160819d5f6f093ff3
Ryan Short <110632551+RyanShort13110@users.noreply.github.com>
2022-08-25 17:56:43 -0400

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,36 +1,36 @@
 // display users name or not
-userName ? console.log(`Hello ${userName}!`) : console.log('Hello!')
+userName ? console.log(`Hello ${userName}!`) : console.log('Hello!');
 
 // setting up the question
-let userQuestion = ''
+let userQuestion = '';
 
 // log the question to user
-console.log(`So, ${userName}, what is your ${userQuestion}?`)
+console.log(`So, ${userName}, what is your ${userQuestion}?`);
 
 // generate random number to pick answer
-let randomNum = Math.floor(Math.random() * 8)
+let randomNum = Math.floor(Math.random() * 8);
 
 // the 8 ball container
-let eightBall = ''
+let eightBall = '';
 
 //the magic
 if(randomNum === 0){
-  eightBall = 'It is certain'
+  eightBall = 'It is certain';
 }else if(randomNum === 1) {
-  eightBall = 'It is decidedly so'
+  eightBall = 'It is decidedly so';
 }else if(randomNum === 2){
-  eightBall = 'Reply hazy, try again'
+  eightBall = 'Reply hazy, try again';
 }else if(randomNum === 3){
-  eightBall = 'Cannot predict now'
+  eightBall = 'Cannot predict now';
 }else if(randomNum === 4){
-  eightBall = 'Do not count on it'
+  eightBall = 'Do not count on it';
 }else if(randomNum === 5){
-  eightBall = 'My sources say no'
+  eightBall = 'My sources say no';
 }else if(randomNum === 6){
-  eightBall = 'Outlook not so good'
+  eightBall = 'Outlook not so good';
 }else if(randomNum === 7){
-  eightBall = 'Signs point to yes'
+  eightBall = 'Signs point to yes';
 }
 
 //log the magic
-console.log(eightBall)
+console.log(eightBall);
